a0db34a1728f3bce299f6cb1cb9216d2287293f7
Jakub Durczok <durczok.jakub@gmail.com>
2026-09-16 14:21:35 +0200

    Update to v0.1.1 slim jar

--- Formula/songs.rb
+++ Formula/songs.rb
@@ -1,9 +1,9 @@
 class Songs < Formula
   desc "Synchronize music playlists"
   homepage "https://github.com/durczokj/songs"
-  url "https://github.com/durczokj/songs/releases/download/v0.1.0/songs.jar"
-  sha256 "2b73d37d1135867c70dac93374e20928fd4a0df85bdf15b36353912e78a0b2af"
-  version "0.1.0"
+  url "https://github.com/durczokj/songs/releases/download/v0.1.1/songs.jar"
+  sha256 "cf3bab25196da4359e31646cf5c27d7ebcc9d38048b1066a9f4ef5320e5ebe09"
+  version "0.1.1"
 
   depends_on "openjdk@21"
   depends_on "ffmpeg"
